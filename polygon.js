07c66336d9219c66a2177a3a8ae0f1a09fb17fac
Michael Laszlo <git@michaellaszlo.com>
2015-04-27 23:45:23 -0400

    Complete segment on mouse out

--- polygon.js
+++ polygon.js
@@ -4,7 +4,6 @@ Polygon.resize = function() {
       canvas = g.canvas;
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  console.log(canvas.width+' '+canvas.height);
 };
 Polygon.load = function () {
   var g = Polygon,
@@ -14,6 +13,27 @@ Polygon.load = function () {
                   end: document.getElementById('end') };
   g.resize();
   window.onresize = g.resize;
+  function makeUnselectable(element) {
+    element.className += ' unselectable';
+    element.ondragstart = element.onselectstart = function (event) {
+      event.preventDefault();
+    };
+  }
+  makeUnselectable(canvas);
+  var numSides = document.getElementById('numSides'),
+      minus = document.getElementById('minus'),
+      plus = document.getElementById('plus');
+  minus.onmousedown = function () {
+    numSides.innerHTML = parseInt(numSides.innerHTML, 10) - 1;
+  };
+  plus.onmousedown = function () {
+    numSides.innerHTML = parseInt(numSides.innerHTML, 10) + 1;
+  };
+  var controls = [display.begin, display.end, numSides, minus, plus,
+                  document.getElementById('options')];
+  for (var i = 0; i < controls.length; ++i) {
+    makeUnselectable(controls[i]);
+  }
   var getPosition = function (event) {
     event = event || window.event;
     var rect = canvas.getBoundingClientRect();
@@ -29,11 +49,13 @@ Polygon.load = function () {
     display.begin.innerHTML = '<span class="label">x1, y1 =</span> '+x1+', '+y1;
     context.lineWidth = 3;
     context.strokeStyle = '#96a93a';
+    for (var i = 0; i < controls.length; ++i) {
+      controls[i].style.zIndex = -10;
+    }
     canvas.onmousemove = function (event) {
       var position = getPosition(event);
       x2 = position.x;
       y2 = position.y;
-      console.log(0, 0, canvas.width, canvas.height);
       context.clearRect(0, 0, canvas.width, canvas.height);
       context.beginPath();
       context.moveTo(x1, y1);
@@ -42,9 +64,13 @@ Polygon.load = function () {
     };
   };
   canvas.onmouseup = canvas.onmouseout = function (event) {
-    console.log('out');
+    if (canvas.onmousemove === undefined) {
+      return;
+    }
     canvas.onmousemove = undefined;
-    canvas.onmouseout = undefined;
+    for (var i = 0; i < controls.length; ++i) {
+      controls[i].style.zIndex = 0;
+    }
     var position = getPosition(event);
     display.end.innerHTML = '<span class="label">x2, y2 =</span> '+x2+', '+y2;
     context.strokeStyle = '#2c562d';
@@ -52,25 +78,6 @@ Polygon.load = function () {
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
-  };
-  function makeUnselectable(element) {
-    element.className += ' unselectable';
-    element.ondragstart = element.onselectstart = function (event) {
-      event.preventDefault();
-    };
-  }
-  makeUnselectable(canvas);
-  var numSides = document.getElementById('numSides'),
-      minus = document.getElementById('minus'),
-      plus = document.getElementById('plus');
-  makeUnselectable(numSides);
-  makeUnselectable(minus);
-  makeUnselectable(plus);
-  minus.onmousedown = function () {
-    numSides.innerHTML = parseInt(numSides.innerHTML, 10) - 1;
-  };
-  plus.onmousedown = function () {
-    numSides.innerHTML = parseInt(numSides.innerHTML, 10) + 1;
   };
 };
 window.onload = Polygon.load;
