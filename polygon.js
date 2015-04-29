24a133e55723c034910424981ed1791ed8a4e373
Michael Laszlo <git@michaellaszlo.com>
2015-04-29 02:55:17 -0400

    Draw polygon

--- polygon.js
+++ polygon.js
@@ -1,4 +1,12 @@
-var Polygon = {};
+var Polygon = {
+  color: {
+    axes: '#ccc',
+    side: {
+       hover: { plain: '#e4e277', special: '#bab850' },
+       final: { plain: '#b3e477', special: '#85a55e' }
+    }
+  }
+};
 Polygon.resize = function() {
   var g = Polygon,
       canvas = g.canvas,
@@ -12,9 +20,10 @@ Polygon.drawAxes = function() {
   var g = Polygon,
       canvas = g.canvas,
       context = g.context,
-      origin = g.origin;
+      origin = g.origin,
+      color = g.color;
   context.lineWidth = 2;
-  context.strokeStyle = '#ddd';
+  context.strokeStyle = color.axes;
   context.beginPath();
   context.moveTo(origin.left, 0);
   context.lineTo(origin.left, canvas.height);
@@ -22,28 +31,52 @@ Polygon.drawAxes = function() {
   context.lineTo(canvas.width, origin.top);
   context.stroke();
 };
-Polygon.drawPolygon = function(x1, y1, x2, y2,
-    strokeStyleFirst, strokeStyleRest) {
+Polygon.turtle = { x: 0, y: 0, angle: 0 };
+Polygon.turtle.setPosition = function (x, y) {
   var g = Polygon,
+      turtle = g.turtle,
+      context = g.context,
+      origin = g.origin;
+  turtle.x = x;
+  turtle.y = y;
+  context.moveTo(origin.left + turtle.x, origin.top - turtle.y);
+};
+Polygon.turtle.setAngle = function (angle) {
+  var g = Polygon,
+      turtle = g.turtle;
+  turtle.angle = angle;
+};
+Polygon.turtle.left = function (turn) {
+  var g = Polygon,
+      turtle = g.turtle;
+  g.turtle.angle = g.normalizeAngle(g.turtle.angle + turn);
+};
+Polygon.turtle.right = function (turn) {
+  var g = Polygon,
+      turtle = g.turtle;
+  g.turtle.angle = g.normalizeAngle(g.turtle.angle - turn);
+};
+Polygon.turtle.forward = function (distance) {
+  var g = Polygon,
+      turtle = g.turtle,
       canvas = g.canvas,
       context = g.context,
       origin = g.origin;
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  g.drawAxes();
-  context.lineWidth = 3;
-  if (strokeStyleFirst !== undefined) {
-    context.strokeStyle = strokeStyleFirst;
-  }
-  context.beginPath();
-  context.moveTo(origin.left + x1, origin.top - y1);
-  context.lineTo(origin.left + x2, origin.top - y2);
-  context.stroke();
-  if (strokeStyleRest !== undefined) {
-    context.strokeStyle = strokeStyleRest;
-  }
-  context.beginPath();
-  context.moveTo(origin.left + x2, origin.top - y2);
-  var n = parseInt(document.getElementById('numSides').innerHTML, 10),
+  turtle.x += Math.cos(turtle.angle)*distance;
+  turtle.y += Math.sin(turtle.angle)*distance;
+  context.lineTo(origin.left + turtle.x, origin.top - turtle.y);
+};
+Polygon.normalizeAngle = function (angle) {
+  angle -= Math.floor(angle/(2*Math.PI))*2*Math.PI;
+  return angle;
+};
+Polygon.drawPolygon = function(x1, y1, x2, y2,
+    strokeStyleWhole, strokeStyleFirst) {
+  var g = Polygon,
+      canvas = g.canvas,
+      context = g.context,
+      turtle = g.turtle,
+      n = parseInt(document.getElementById('numSides').innerHTML, 10),
       turn = 2*Math.PI / n,
       dx = x2-x1,
       dy = y2-y1,
@@ -53,14 +86,35 @@ Polygon.drawPolygon = function(x1, y1, x2, y2,
   } else {
     var angle = 2*Math.PI - Math.acos(dx/length);
   }
-  console.log(Math.floor(180*angle/Math.PI));
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  g.drawAxes();
+  context.lineWidth = 4;
+  context.beginPath();
+  if (strokeStyleWhole != undefined) {
+    context.strokeStyle = strokeStyleWhole;
+  }
+  turtle.setPosition(x1, y1);
+  turtle.setAngle(angle);
+  for (var i = 0; i < n; ++i) {
+    turtle.forward(length);
+    turtle.left(turn);
+  }
+  context.stroke();
+  if (strokeStyleFirst !== undefined) {
+    context.strokeStyle = strokeStyleFirst;
+    context.beginPath();
+    turtle.setPosition(x1, y1);
+    turtle.forward(length);
+    context.stroke();
+  }
 }
 Polygon.load = function () {
   var g = Polygon,
       canvas = g.canvas = document.getElementById('surface'),
       context = g.context = canvas.getContext('2d'),
       display = { begin: document.getElementById('begin'),
-                  end: document.getElementById('end') };
+                  end: document.getElementById('end') },
+      color = g.color;
   g.resize();
   window.onresize = g.resize;
   function makeUnselectable(element) {
@@ -74,10 +128,16 @@ Polygon.load = function () {
       minus = document.getElementById('minus'),
       plus = document.getElementById('plus');
   minus.onmousedown = function () {
-    numSides.innerHTML = parseInt(numSides.innerHTML, 10) - 1;
+    var current = parseInt(numSides.innerHTML, 10);
+    if (current > 3) {
+      numSides.innerHTML = current-1;
+    }
   };
   plus.onmousedown = function () {
-    numSides.innerHTML = parseInt(numSides.innerHTML, 10) + 1;
+    var current = parseInt(numSides.innerHTML, 10);
+    if (current < 20) {
+      numSides.innerHTML = current+1;
+    }
   };
   var controls = [display.begin, display.end, numSides, minus, plus,
                   document.getElementById('options')];
@@ -102,7 +162,8 @@ Polygon.load = function () {
     y1 = y2 = position.y;
     display.begin.innerHTML = '<span class="label">x1, y1 =</span> '+x1+', '+y1;
     display.end.innerHTML = '';
-    Polygon.drawPolygon(x1, y1, x2, y2, '#96a93a');
+    Polygon.drawPolygon(x1, y1, x2, y2,
+                        color.side.hover.plain, color.side.hover.special);
     for (var i = 0; i < controls.length; ++i) {
       controls[i].style.zIndex = -10;
     }
@@ -111,7 +172,8 @@ Polygon.load = function () {
       x2 = position.x;
       y2 = position.y;
       display.end.innerHTML = '<span class="label">x2, y2 =</span> '+x2+', '+y2;
-      Polygon.drawPolygon(x1, y1, x2, y2, '#96a93a');
+      Polygon.drawPolygon(x1, y1, x2, y2,
+                          color.side.hover.plain, color.side.hover.special);
     };
   };
   function noop() {
@@ -122,7 +184,8 @@ Polygon.load = function () {
       return;
     }
     canvas.onmousemove = noop;
-    Polygon.drawPolygon(x1, y1, x2, y2, '#2c562d');
+    Polygon.drawPolygon(x1, y1, x2, y2,
+                        color.side.final.plain, color.side.final.special);
     for (var i = 0; i < controls.length; ++i) {
       controls[i].style.zIndex = 0;
     }
