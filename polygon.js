3a93a15d378100e48556d86cab19a813e93022fc
Michael Laszlo <git@michaellaszlo.com>
2015-04-28 23:59:25 -0400

    Calculate turn and length

--- polygon.js
+++ polygon.js
@@ -5,10 +5,38 @@ Polygon.resize = function() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 };
+Polygon.draw = function(x1, y1, x2, y2, strokeStyleFirst, strokeStyleRest) {
+  var g = Polygon,
+      canvas = g.canvas,
+      context = g.context;
+  if (strokeStyleFirst !== undefined) {
+    context.strokeStyle = strokeStyleFirst;
+  }
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.beginPath();
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  if (strokeStyleRest !== undefined) {
+    context.strokeStyle = strokeStyleRest;
+  }
+  context.moveTo(x2, y2);
+  var n = parseInt(document.getElementById('numSides').innerHTML, 10),
+      turn = 2*Math.PI / n,
+      dx = x2-x1,
+      dy = y2-y1,
+      length = Math.sqrt(dx*dx, dy*dy);
+  if (dy >= 0) {
+    var angle = Math.asin(dy/length);
+  } else {
+    var angle = 2*Math.PI - Math.acos(dx/length);
+  }
+  console.log(angle);
+}
 Polygon.load = function () {
   var g = Polygon,
       canvas = g.canvas = document.getElementById('surface'),
-      context = canvas.getContext('2d'),
+      context = g.context = canvas.getContext('2d'),
       display = { begin: document.getElementById('begin'),
                   end: document.getElementById('end') };
   g.resize();
@@ -39,16 +67,16 @@ Polygon.load = function () {
     var rect = canvas.getBoundingClientRect();
     return { x: event.clientX - rect.left, y: event.clientY - rect.top };
   };
+  context.lineWidth = 3;
   var x1, y1, x2, y2;
   canvas.onmousedown = function (event) {
     document.body.style.cursor = 'default';
     var position = getPosition(event);
     x1 = x2 = position.x;
     y1 = y2 = position.y;
-    display.end.innerHTML = '';
     display.begin.innerHTML = '<span class="label">x1, y1 =</span> '+x1+', '+y1;
-    context.lineWidth = 3;
-    context.strokeStyle = '#96a93a';
+    display.end.innerHTML = '';
+    Polygon.draw(x1, y1, x2, y2, '#96a93a');
     for (var i = 0; i < controls.length; ++i) {
       controls[i].style.zIndex = -10;
     }
@@ -56,11 +84,8 @@ Polygon.load = function () {
       var position = getPosition(event);
       x2 = position.x;
       y2 = position.y;
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.beginPath();
-      context.moveTo(x1, y1);
-      context.lineTo(x2, y2);
-      context.stroke();
+      display.end.innerHTML = '<span class="label">x2, y2 =</span> '+x2+', '+y2;
+      Polygon.draw(x1, y1, x2, y2, '#96a93a');
     };
   };
   function noop() {
@@ -71,16 +96,10 @@ Polygon.load = function () {
       return;
     }
     canvas.onmousemove = noop;
+    Polygon.draw(x1, y1, x2, y2, '#2c562d');
     for (var i = 0; i < controls.length; ++i) {
       controls[i].style.zIndex = 0;
     }
-    var position = getPosition(event);
-    display.end.innerHTML = '<span class="label">x2, y2 =</span> '+x2+', '+y2;
-    context.strokeStyle = '#2c562d';
-    context.beginPath();
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
-    context.stroke();
   };
 };
 window.onload = Polygon.load;
